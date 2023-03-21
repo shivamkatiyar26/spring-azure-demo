192fccd7f396d993248fb3469f17bfe8a60b71d3
ts-shivam.katiyar <ts-shivam.katiyar@rakuten.com>
2023-03-21 13:56:10 +0530

    first commit

---
+++ src/main/java/com/example/springazuredemo/SpringAzureDemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.springazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats, App is deployed";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
